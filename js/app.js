a19dc52c07393627de851b24048ba4a15575598c
Jakub Paluszkiewicz <jakub.k.paluszkiewicz@gmail.com>
2018-10-09 19:46:27 +0200

    additional object

--- js/app.js
+++ js/app.js
@@ -2,5 +2,13 @@ new Vue({
     el: '#app',
     data: {
         attachRed: false
+    },
+    computed: {
+        divClasses() {
+            return {
+                'red': this.attachRed,
+                'blue': !this.attachRed
+            }
+        }
     }
 });
