7c991b380a84a2cfd8ae92fd6467f6daafaa1927
Jakub Paluszkiewicz <jakub.k.paluszkiewicz@gmail.com>
2018-10-09 19:07:59 +0200

    computed properties

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 new Vue({
     el: '#app',
     data: {
-        name: "Kuba"
+        counter: 0,
+        result: '',
+        secondCounter: 0
+    },
+    computed: {
+        output: function () {
+            console.log('Computed');
+            return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+        }
+    },
+    methods: {
+        showResult() {
+            console.log('Method');
+            return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+        }
     }
 });
