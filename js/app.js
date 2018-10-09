f446bc4984bf23d67ba241455280922702ba07a8
Jakub Paluszkiewicz <jakub.k.paluszkiewicz@gmail.com>
2018-10-09 19:42:53 +0200

    manage classes

--- js/app.js
+++ js/app.js
@@ -1,29 +1,6 @@
 new Vue({
     el: '#app',
     data: {
-        counter: 0,
-        result: '',
-        secondCounter: 0
-    },
-    computed: {
-        output: function () {
-            console.log('Computed');
-            return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
-        }
-    },
-    watch: {
-       counter: function (value) {
-           let vm = this;
-           setTimeout(function () {
-               vm.counter = 0;
-           }, 2000)
-       }
-        
-    },
-    methods: {
-        showResult() {
-            console.log('Method');
-            return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
-        }
+        attachRed: false
     }
 });
