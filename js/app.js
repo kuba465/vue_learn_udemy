5057b47c24ba731eab5050149267e5b8989b77e4
Jakub Paluszkiewicz <jakub.k.paluszkiewicz@gmail.com>
2018-10-10 18:59:50 +0200

    basic list

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 new Vue({
     el: '#app',
     data: {
-        show: true
+        ingredients: ['meat', 'fruit', 'vegetables'],
+        persons: [
+            {name: 'Kuba', age: 27, color: 'red'},
+            {name: 'Dona', age: 28, color: 'blue'}
+        ]
     }
 });
